574580df3f041a96d01169b68b5014187f779f5f
Steve-VE <steve.vanessche@gmail.com>
2018-02-21 10:24:26 +0100

    Support l'ajout et l'archivage de Todo

--- index.php
+++ index.php
@@ -1,13 +1,22 @@
 <?php
 require "php/functions.php";
 
-if(isset($_POST)){
-    var_dump($_POST);
-    if(isset($_POST['new_entry']) && !empty(isset($_POST['new_entry']))){
-        $new_entry = get_value( "new_entry" );
-        add_to_JSON($new_entry);
+$to_archive = null;
 
-        
+
+if(isset($_POST)){
+    // var_dump($_POST);
+
+    if(isset($_POST['todo_to_archive'])){
+        if(is_array($_POST['todo_to_archive'])){    
+            $to_archive = $_POST['todo_to_archive'];
+            archive_json($to_archive);
+        }
+    }
+
+    if(isset($_POST['new_entry']) && !empty(isset($_POST['new_entry']))){
+        // $new_entry = get_value( "new_entry_text" );
+        add_to_json( get_value( "new_entry" ) );
     }
 }
 
@@ -25,12 +34,59 @@ if(isset($_POST)){
     <title>Todo List</title>
 </head>
 <body>
-    
-    <form action="" method="post">
-        <!-- <textarea name="new_entry" id="blocnote" ></textarea> -->
-        <input type="text" name="new_entry" id="">
-        <input type="submit" value="Enregistrer">
-    </form>
+    <main>
+        <form class="create-todo" action="" method="post">
+            <h1>Ajouter une nouvelle tâche</h1>
+            <input type="text" name="new_entry" id="new-entry">
+            <input type="submit" value="Enregistrer">
+        </form>
 
+
+        <div class="list">
+            <h2>À faire</h2>
+            <form action="" method="post">
+                <?php
+                $data = load_json();
+                
+                for($i = 0; $i < count($data); $i++){
+                    $current_item = $data[$i];
+                    
+                    if($current_item->archived == false){
+                        echo '<input type="checkbox" name="todo_to_archive[]" value ="';
+                        echo $i;
+                        echo '" />';
+
+                        echo ' <label for="'. $i .'">';
+                        echo $current_item->content;
+                        echo '</label><br/>';
+                    }
+                }
+                ?>
+                <input type="submit" value="Archiver">
+            </form>
+        </div>
+        <div class="list">
+            <h2>Archivés</h2>
+            <ul>
+                <?php
+                $data = load_json();
+                
+                for($i = 0; $i < count($data); $i++){
+                    $current_item = $data[$i];
+                    
+                    if($current_item->archived == true){
+                        echo '<li>';
+                        echo $current_item->content;
+                        echo '</li>';
+                    }
+                }
+                ?>
+            </ul>
+        </div>
+    </main>
+
+    <?php
+    var_dump($_POST);
+    ?>
 </body>
 </html>
--- php/functions.php
+++ php/functions.php
@@ -1,28 +1,63 @@
 <?php
 
-function add_to_JSON( $value, $url="assets/json/datalist.json"){
-    $raw_file = file_get_contents( $url );
-    $decoded_file = json_decode($raw_file, true);
+// Ajoute une valeur à un fichier JSON
+function add_to_json( $text, $url="assets/json/datalist.json"){
+    // Partie décodage
+    $array_data = [];
+    $array_data = load_json( $url );
 
-    $decoded_file = $value;
-    $raw_file = json_encode($decoded_file);
 
-    // Partie encodage
-    $array_test[] = [
-        "title" => "TODO #1",
-        "content" => $value
-    ];
+    if( $text != "" && $text != false && $text != null ){
 
-    file_put_contents($url, json_encode( $array_test ));
+        $array_data[] = [
+            "content" => $text,
+            "archived" => false
+        ];
+
+        // Sauvegarde du fichier
+        file_put_contents($url, json_encode( $array_data, JSON_PRETTY_PRINT ));
+    }
 }
 
+
+// Charge et retourne un fichier JSON
+function load_json($url="assets/json/datalist.json"){
+    if(!$raw_file = file_get_contents($url)){
+        $error = error_get_last();
+        // echo "HTTP request failed. Error was: " . $error['message'];
+    }
+    else{
+        return json_decode($raw_file);
+    }
+}
+
+// Fonction à appeller pour "archiver" des valeurs dans le fichier JSON
+function archive_json($index_to_archive, $url="assets/json/datalist.json"){
+    $array_data = load_json( $url );
+
+    echo 'VAR DUMP : '; var_dump($index_to_archive);
+    if(isset($index_to_archive) && count($index_to_archive) > 0){
+
+        for($i = 0; $i < count($array_data); $i++){
+            if(in_array( $i, $index_to_archive )){
+                $array_data[$i]->archived = true;
+            }
+        }
+    }
+
+    file_put_contents($url, json_encode( $array_data, JSON_PRETTY_PRINT ));
+}
+
+
+// Retourne une valeur à aller chercher dans $_POST
 function get_value($value_name){
     $value_to_return = null;
 
     if(isset($_POST[$value_name])){
         $value_to_return = filter_var($_POST[$value_name], FILTER_SANITIZE_STRING);
+        // unset($_POST[$value_name]); // On détruit la variable contenu dans POST après l'avoir récupéré pour éviter de la renvoyer en rechargeant la page
     }
-    $value_to_return = $_POST[$value_name];
+
     return $value_to_return;
 }
 
